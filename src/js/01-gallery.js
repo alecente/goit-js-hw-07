6758c0a29fd1d0287f0ff710ce472f8bb9631cb2
Alejandra Centellas <alejandracentellasr@gmail.com>
2023-04-11 21:22:24 -0500

    Final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+let myGallery = document.querySelector(".gallery");
+let instanceBlib = "";
+
+function onEscape(evt) {
+  if (evt.code === "Escape") {
+    if (basicLightbox.visible()) {
+      instanceBlib.close();
+    }
+  }
+  // console.log(evt);
+}
+
+function showModalWindow(myLink) {
+  instanceBlib = basicLightbox.create(
+    `<img width="1400" height="900" src="${myLink}"></img>`,
+    {
+      onShow: (instanceBlib) => document.addEventListener("keydown", onEscape),
+      onClose: (instanceBlib) =>
+        document.removeEventListener("keydown", onEscape),
+    }
+  );
+  instanceBlib.show();
+}
+
+function galleryOnClick(evt) {
+  evt.preventDefault();
+  let targetLink = evt.target.dataset.source;
+  if (!targetLink) {
+    alert("¡No se ha encontrado ningún enlace de imagen!");
+  } else {
+    showModalWindow(targetLink);
+  }
+  //   console.log(evt.target.dataset.source);
+}
+
+myGallery.addEventListener("click", galleryOnClick);
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }, idx) =>
+      `<div class = "gallery__item" >
+      <a class = "gallery__link" href = "${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source = "${original}"
+  alt="${description}">
+  </a>
+  </div>`
+  )
+  .join("");
+
+myGallery.insertAdjacentHTML("beforeend", markup);
+
+//console.log(basicLightbox);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+let myGallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }, idx) =>
+      `<a class = "gallery__item" href = "${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}">
+  </a>`
+  )
+  .join("");
+
+myGallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  //   captionPosition: "bottom", //default = "bottom"
+  captionDelay: 250,
+});
+
+// console.log(lightbox);
